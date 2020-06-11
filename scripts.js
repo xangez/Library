6dbcd7ab9de46993eda8b1237a2c9bbd69251919
xangez <xangez348@gmail.com>
2020-06-11 21:46:08 +1000

    added local storage

--- scripts.js
+++ scripts.js
@@ -1,15 +1,12 @@
-
 const inputs = document.querySelector("form").elements;
-
 const addBookbtn = document.querySelector("#add-book");
 addBookbtn.addEventListener("click", openForm);
-
 const shelf = document.querySelector("#shelf");
-
 const submit = document.querySelector(".submit");
 submit.addEventListener("click", addBookToLibrary);
+const cardTemplate = document.querySelector("#card-template");
 
-myLibrary = [];
+myLibrary = JSON.parse(localStorage.getItem("library.books")) || [];
 
 function openForm() {
   computedDisplay = window.getComputedStyle(form).getPropertyValue('display');
@@ -23,20 +20,6 @@ function openForm() {
   }
 }
 
-/*
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.pushtoMyLibrary();
-}
-
-Book.prototype.pushtoMyLibrary = function() {
-  myLibrary.push(this);
-}
-*/
-
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
@@ -46,8 +29,6 @@ class Book {
   }
 }
 
-
-
 function addBookToLibrary() {
   let title = inputs[0].value;
   let author = inputs[1].value;
@@ -55,78 +36,81 @@ function addBookToLibrary() {
   let checked = inputs[3].checked ? "Read" : "Not read";
   let newBook = new Book(title, author, pages, checked);
   myLibrary.push(newBook);
+  updateAndRender();
+}
+
+function updateAndRender() {
+  updateStorage();
   render();
 }
 
 function render() {
-  let book = myLibrary[myLibrary.length - 1]
+  clearShelf();
+  myLibrary.forEach(book => {
+    const cardElement = document.importNode(cardTemplate.content, true);
+    const card = cardElement.querySelector('.card');
+    card.id = book.title;
+    const cardTitle = cardElement.querySelector('.cardTitle');
+    cardTitle.textContent = book.title;
+    const cardAuthor = cardElement.querySelector('.cardAuthor');
+    cardAuthor.textContent = book.author;
+    const cardPages = cardElement.querySelector('.cardPages');
+    cardPages.textContent = book.pages;
 
-  let bookCard = document.createElement('div');
-  bookCard.id = book.title;
+    const cardRead = cardElement.querySelector('.cardRead')
+    cardRead.textContent = book.read;
+    cardRead.textContent == "Read" ? 
+    cardRead.classList.add('toggleRead') : 
+    cardRead.classList.add('toggleNotRead');
+    cardRead.addEventListener("click", toggleRead);
 
-  //title
-  let cardTitle = document.createElement('span');
-  cardTitle.textContent = book.title;
-  cardTitle.classList.add("cardTitle");
-  bookCard.appendChild(cardTitle);
+    const removeBtn = cardElement.querySelector('.removeBtn')
+    removeBtn.addEventListener("click", removeCard);
+    shelf.appendChild(cardElement);
+  })
+}
 
-  //author
-  let cardAuthor = document.createElement('span');
-  cardAuthor.textContent = book.author;
-  cardAuthor.classList.add("cardAuthor");
-  bookCard.appendChild(cardAuthor);
-
-  //cardPages
-  let cardPages = document.createElement('span');
-  cardPages.textContent = book.pages;
-  cardPages.classList.add("cardPages");
-  bookCard.appendChild(cardPages);
-
-  //cardRead
-  let cardRead = document.createElement('button');
-  cardRead.textContent = book.read;
-  cardRead.classList.add("cardRead");
-  cardRead.textContent == "Read" ? 
-    cardRead.style.backgroundColor = "rgb(175, 169, 134)" : 
-    cardRead.style.backgroundColor = "rgb(190, 161, 161)";
-  bookCard.appendChild(cardRead);
-  cardRead.addEventListener("click", toggleRead);
-
-  //removeCard
-  let removeBtn = document.createElement("button");
-  removeBtn.textContent = "X";
-  removeBtn.classList.add("removeBtn");
-  bookCard.appendChild(removeBtn);
-  removeBtn.addEventListener("click", removeCard);
-
-  //append card
-  bookCard.classList.add("card");
-  shelf.appendChild(bookCard);
+function clearShelf() {
+  while (shelf.firstChild) {
+    shelf.removeChild(shelf.firstChild)
+  }
 }
 
 function toggleRead(e){
   if (e.target.textContent == "Not read") {
     e.target.textContent = "Read";
-    e.target.style.backgroundColor = "rgb(175, 169, 134)";
+    e.target.classList.remove('toggleNotRead');
+    e.target.classList.add('toggleRead');
   }
   else {
     e.target.textContent = "Not read";
-    e.target.style.backgroundColor = "rgb(190, 161, 161)";
+    e.target.classList.remove('toggleRead');
+    e.target.classList.add('toggleNotRead');
   }
 }
 
 function removeCard(e) {
   let book = e.target.parentNode;
   let id = book.id;
-  book.remove();
   myLibrary.forEach((book, index) => {
     if (book.title == id) {
       myLibrary.splice(index, 1);
     }
   });
+  updateAndRender();
 }
 
+function populateMyLibrary() {
+  myLibrary.push(new Book("The Hobbit", "J.R.R Tolkien", '200', 'Read'));
+  myLibrary.push(new Book("The Final Empire", "Brandon Sanderson", '200', 'Not Read'));
+}
 
-let thehobbit = new Book("The Hobbit", "J.R.R Tolkien", '200', 'Read');
-myLibrary.push(thehobbit);
-render();
+function updateStorage() {
+  localStorage.setItem("library.books", JSON.stringify(myLibrary));
+}
+
+function onload() {
+  render();
+}
+
+onload();
